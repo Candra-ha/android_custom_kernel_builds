a8a516e0f04d00220e01249e2b21d7dadd2ea1df
Candra-ha <situshalal1@gmail.com>
2026-05-10 18:05:41 +0700

    Apply DroidSpaces kernel patch and set config flags
    Added kernel patch application and configuration flags for IPC and namespace support.

--- actions/peridot/entrypoint.sh
+++ actions/peridot/entrypoint.sh
@@ -67,10 +67,17 @@ config_file="$workdir/arch/arm64/configs/vendor/peridot_GKI.config"
 #cp -r susfs/kernel_patches/include/linux/* include/linux/
 #cp -r susfs/kernel_patches/fs/* fs/
 
+msg "Applying DroidSpaces kernel patch"
+PATCH_URL="https://raw.githubusercontent.com/ravindu644/Droidspaces-OSS/main/Documentation/resources/kernel-patches/GKI/below-kernel-6.12/001.GKI-below-6.12-fix_sysvipc_kabi_6_7_8.patch"
+PATCH_FILE="/tmp/001.GKI-below-6.12-fix_sysvipc_kabi_6_7_8.patch"
+wget -q --no-check-certificate "$PATCH_URL" -O "$PATCH_FILE"
+patch -p1 < "$PATCH_FILE"
+
 #set_config_flag CONFIG_KPROBES "$config_file"
 #set_config_flag CONFIG_HAVE_KPROBES "$config_file"
 #set_config_flag CONFIG_KPROBE_EVENTS "$config_file"
 #set_config_flag CONFIG_KSU "$config_file"
+
 #set_config_flag CONFIG_KSU_SUSFS "$config_file"
 #set_config_flag CONFIG_KSU_SUSFS_SUS_MOUNT "$config_file"
 #set_config_flag CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG "$config_file"
@@ -80,6 +87,12 @@ config_file="$workdir/arch/arm64/configs/vendor/peridot_GKI.config"
 #set_config_flag CONFIG_KSU_SUSFS_SPOOF_UNAME "$config_file"
 #set_config_flag CONFIG_KSU_SUSFS_OPEN_REDIRECT "$config_file"
 #set_config_flag CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS "$config_file"
+
+set_config_flag CONFIG_SYSVIPC "$config_file"
+set_config_flag CONFIG_POSIX_MQUEUE "$config_file"
+set_config_flag CONFIG_IPC_NS "$config_file"
+set_config_flag CONFIG_PID_NS "$config_file"
+set_config_flag CONFIG_DEVTMPFS "$config_file"
 
 msg "Downloading toolchain"
 #mkdir toolchain && (cd toolchain; bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S)
