5a7e69b8324d27f8db801707e0d738cd95437d83
Candra-ha <situshalal1@gmail.com>
2026-06-11 20:33:05 +0700

    Refactor kernel build commands in entrypoint.sh

--- actions/peridot/entrypoint.sh
+++ actions/peridot/entrypoint.sh
@@ -101,26 +101,13 @@ export SUBARCH=arm64
 export DISABLE_WRAPPER=1
 export BUILD_CC="$(pwd)/toolchain/bin/clang"
 export PATH="$(pwd)/toolchain/bin/:$PATH"
+KERNEL_DEFCONFIG="gki_defconfig vendor/pineapple_GKI.config vendor/peridot_GKI.config"
+KERNEL_CMDLINE="ARCH=arm64 CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld CROSS_COMPILE=aarch64-linux-gnu- O=out LLVM=1 LLVM_IAS=1"
 
-make O=out ARCH=arm64 gki_defconfig vendor/pineapple_GKI.config vendor/peridot_GKI.config
+make $KERNEL_CMDLINE $KERNEL_DEFCONFIG 
 
 eattime () {
-make -j$(nproc --all) O=out LLVM=1 LLVM_IAS=1 \
-ARCH=arm64 \
-CC=clang \
-LD=ld.lld \
-AR=llvm-ar \
-AS=llvm-as \
-NM=llvm-nm \
-STRIP=llvm-strip \
-OBJCOPY=llvm-objcopy \
-OBJDUMP=llvm-objdump \
-READELF=llvm-readelf \
-HOSTCC=clang \
-HOSTCXX=clang++ \
-HOSTAR=llvm-ar \
-HOSTLD=ld.lld \
-CROSS_COMPILE=aarch64-linux-gnu-
+make $KERNEL_CMDLINE -j$(nproc --all)
 }
 
 eattime 2>&1 | tee -a out/logs/compile.log
